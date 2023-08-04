0671042bbcbbfb770f97f7f0a50e4fb15a54d752
Tanyo Nikolov <tanyo.nikolov@MBGNM0138.local>
2023-08-04 20:22:59 +0300

    add static

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const webPush = require("web-push");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./"));
+
+app.listen(5000);
 
 const vapidKeys = {
   publicKey:
@@ -12,7 +19,16 @@ webPush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-const subscription = {};
+const subscription = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dcxo2J0C25o:APA91bErvjpozl7aTb5Vq0BXPiB6Lq8fVCVC6IzonvBg6HjZ5aX00p-uiOxNLbqWjuHu1KbyDClambDZm-tqBkpHRzZIJLIfnHcJtHPuVuA0ZU1YaiH0wUTbd3NebMhN-ZuAlDOYqok3",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BPracTC6BfSJ6VfVlKxMevGvrp3ybsUYX1EVJ8PYo49_mBXU-p4WuEhIOUFUEZrK2oL1b8Mzd32sl2CZubGe3cE",
+    auth: "JP60jxh8eXkuYQwddjlNaw",
+  },
+};
 
 async function send() {
   await webPush.sendNotification(subscription, "Testing push notifications");
